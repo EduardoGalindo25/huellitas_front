350147496dded866c0f2e640a93da6107d83d552
heli <heli2.mdz@gmail.com>
2025-04-30 11:46:12 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Servicios from "./Screens/Servicios";
+import NavBar from "../src/Components/NavBar";
+
+function App() {
+  return (
+      <Servicios/>
+
+  );
+}
+
+
+/*const App = () => {
+  return <NavBar/>;
+};*/
+
+export default App;
+
