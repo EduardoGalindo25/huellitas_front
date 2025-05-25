450fd84f3df3d2b0e6383ab861317c22dbf4d4bf
Gabriel <eduardogalro32@gmail.com>
2025-05-25 13:28:20 -0600

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import Login from "../components/LoginForm";
+import Navbar from "../components/navbarServicios";
+import Footer from "../components/Footer";
+
+function LoginPage() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+      <Footer />
+    </div>
+  );
+}
+
+export default LoginPage;
